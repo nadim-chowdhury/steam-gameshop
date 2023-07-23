64e66526774509ec68c88ff5a318c16af18b5f4c
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-23 19:22:37 +0600

    redux toolkit implemented

--- src/app/features/gameDataSlice.js
+++ src/app/features/gameDataSlice.js
@@ -1,7 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const counterSlice = createSlice({
+const initialState = {
+  loading: false,
+  data: [],
+  error: "",
+};
+
+export const fetchGames = createAsyncThunk("games/fetchGames", () => {
+  return axios.get(
+    `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${
+      import.meta.env.VITE_STEAM_API_KEY
+    }&steamids=76561197960435530`
+  ).then(res => );
+});
+
+export const gameDataSlice = createSlice({
   name: "gameData",
-  initialState: [],
+  initialState,
   reducers: {},
 });
+
+export const {} = gameDataSlice.actions;
+
+export default gameDataSlice.reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { gameDataSlice } from "./features/gameDataSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    gameDataReducer: gameDataSlice,
+  },
 });
