99970b00269dbbc49a043f36d8a641990aece713
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-23 18:53:49 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <></>;
+  return (
+    <main className="h-screen">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/app/features/gameDataSlice.js
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "gameData",
+  initialState: [],
+  reducers: {},
+});
